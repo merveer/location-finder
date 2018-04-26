c4e965418679de33eeb6c497e4018878f280cab3
Merve Erdol <merve-uygun@windowslive.com>
2018-04-26 12:14:45 +0300

    Error handling

--- app.js
+++ app.js
@@ -20,8 +20,16 @@ request({
     url: `http://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
     json:true
 }, (error, response, body) => {
-    console.log(`Address: ${body.results[0].formatted_address}`);
-    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+    if(error){
+        console.log("Unable to conect google servers");
+    }
+    else if(body.status === "ZERO_RESULTS") {
+        console.log("Unable to find address ");
+    } else if(body.status === "OK") {
+        console.log("Here is address details");
+        console.log(`Address: ${body.results[0].formatted_address}`);
+        console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+        console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+    }
 });
 
