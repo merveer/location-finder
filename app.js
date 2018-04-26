cc75c2de1d0b266a91651fc06ebb39a567472113
Merve Erdol <merve-uygun@windowslive.com>
2018-04-26 10:38:58 +0300

    Use yargs to setup configuration

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
 const request = require("request");
+const yargs = require("yargs");
+
+const argv = yargs // obj that stores final parsed output
+    .options({
+        a: {
+            demand: true,
+            alias: "address",
+            describe: "Address to fetch weather for",
+            string: true
+        }
+    })
+    .help()
+    .alias("help", "h")
+    .argv;
+
+console.log(argv);
 
 request({
     url: "http://maps.googleapis.com/maps/api/geocode/json?address=cankaya",
